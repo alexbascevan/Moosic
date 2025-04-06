cb60e7a6cd5f66b81485ceedddc8b37a49395514
Mariah Falzon <133882225+tobeytheghost@users.noreply.github.com>
2025-04-06 13:59:14 -0400

    Add files via upload
    This project file contains MVC structure and models for database

---
+++ Potatoes_GroupProject/Moosic/Models/Music.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Moosic.Models
+{
+    /*
+     * Group Name: Potatoes
+     * Project Name: Moosic
+     * 
+     * Created By: Mariah Falzon 
+     * 
+     * Created On: April 6, 2025
+     * Updated On: 
+     * 
+     * Purpose: Class to handle specific music information from the API
+     * 
+     * API: https://reccobeats.com/docs/apis/get-recommendation
+     */
+    public class Music
+    {
+        [Key]
+        public int Id {  get; set; }    
+
+        public string? Title { get; set; }
+
+        public string? Artist { get; set; }
+
+        public string? Album { get; set; }
+
+        public string? Genre { get; set; }
+
+        public string? ApiId { get; set; } //this will hold any of the IDs from the Recco Beats API
+    }
+}
