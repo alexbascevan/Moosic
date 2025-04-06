6617f463425b534fc9040b861b13767114e0bcfd
Mariah Falzon <133882225+tobeytheghost@users.noreply.github.com>
2025-04-06 17:51:47 -0400

    Add files via upload

---
+++ Potatoes_GroupProject/Moosic/Models/MoosicDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Moosic.Models
+{
+
+    /*
+* Group Name: Potatoes
+* Project Name: Moosic
+* 
+* Created By: Mariah Falzon 
+* 
+* Created On: April 6, 2025
+* Updated On: 
+* 
+* Purpose: Database context to handle database creation
+* 
+* API: https://reccobeats.com/docs/apis/get-recommendation
+*/
+    public class MoosicDbContext : DbContext
+    {
+
+        public MoosicDbContext(DbContextOptions<MoosicDbContext> options) : base(options) { }
+
+        //Entity 
+        public DbSet<User> Users { get; set; }
+
+        //Entity 
+        public DbSet<LibraryItem> LibraryItems { get; set; }
+
+        //Entity
+        public DbSet<Music> MusicItems { get; set; }
+
+    }
+}
